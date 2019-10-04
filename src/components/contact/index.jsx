3387dc809f5c9ea0d66b5176a3bfb114cd5cd5c3
alidnd <alidindi@gmx.de>
2019-10-04 08:46:49 +0200

    Form Validation added on Contact Form

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  ValidationForm,
+  TextInput,
+  TextInputGroup,
+} from 'react-bootstrap4-form-validation';
+
 import classNames from 'classnames/bind';
 import css from './styles.css';
 
@@ -14,13 +20,32 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
 
+    this.formRef = React.createRef();
     this.state = {
       name: '',
       email: '',
       subject: '',
       message: '',
+      immediate: true,
+      setFocusOnError: true,
+      clearInputOnReset: false,
     };
   }
+
+  handleSubmit = (e, formData, inputs) => {
+    e.preventDefault();
+    console.log(formData);
+    alert(JSON.stringify(formData, null, 2));
+  };
+
+  handleErrorSubmit = (e, formData, errorInputs) => {
+    console.log(e, formData, inputs);
+  };
+
+  resetForm = () => {
+    const formRef = this.formRef.current;
+    formRef.resetValidationState(this.state.clearInputOnReset);
+  };
 
   handleFormSubmit(e) {
     e.preventDefault();
@@ -64,7 +89,89 @@ export default class Contact extends Component {
         <div className="container">
           <form>
             <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Name</label>
+              <ValidationForm
+                onSubmit={this.handleSubmit}
+                onErrorSubmit={this.handleErrorSubmit}
+                ref={this.formRef}
+                immediate={this.state.immediate}
+                setFocusOnError={this.state.setFocusOnError}
+                defaultErrorMessage={{ required: 'Please enter something.' }}
+              >
+                <div className="form-group">
+                  <label htmlFor="fullName">Name</label>
+                  <TextInputGroup
+                    name="fullName"
+                    id="fullName"
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    value={this.state.name}
+                    onChange={e => this.setState({ name: e.target.value })}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <TextInputGroup
+                    name="email"
+                    id="email"
+                    type="email"
+                    placeholder="E-Mail"
+                    value={this.state.email}
+                    onChange={e => this.setState({ email: e.target.value })}
+                    successMessage="Looks good!"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="fullName">Subject</label>
+                  <TextInputGroup
+                    name="subject"
+                    id="subject"
+                    type="text"
+                    className="form-control"
+                    placeholder="Subject"
+                    value={this.state.subject}
+                    onChange={e => this.setState({ subject: e.target.value })}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="fullName">Message</label>
+                  <TextInput
+                    multiline required
+                    name="message"
+                    id="message"
+                    type="text"
+                    className="form-control"
+                    placeholder="Message"
+                    rows="5"
+                    value={this.state.message}
+                    onChange={e => this.setState({ message: e.target.value })}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className={bannerButton}
+                  onClick={e => this.handleFormSubmit(e)}
+                >
+                  Submit
+                </button>
+                <button>Submit</button>
+                <button
+                  type="button"
+                  onClick={() => this.resetForm()}
+                >
+                  Reset
+                </button>
+                <button
+                  type="submit"
+                  className={bannerButton}
+                  onClick={e => this.handleFormSubmit(e)}
+                >
+                  Submit
+                </button>
+              </ValidationForm>
               <input
                 type="email"
                 ref="name"
